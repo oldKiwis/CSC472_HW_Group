6b417957c87b283f180a2054a05482e98a6c87bc
Chrisbarbosa711 <chrisbarbosa711@gmail.com>
2026-02-20 21:25:12 -0500

    updated and corrected code
    removed unnecessary matrix operations that caused stretching

--- HW1acode.cpp
+++ HW1acode.cpp
@@ -82,11 +82,26 @@ HW1a::initializeGL()
 void
 HW1a::resizeGL(int w, int h)
 {
+	float ar = (float) w / h;
+
+    float xmax, ymax;
+    if(ar > 1.0){
+        xmax = ar;
+        ymax = 1.0;
+    }
+    else {
+        xmax = 1.0;
+        ymax = 1.0 / ar;
+    }
+
 	// // set viewport to occupy full canvas initially 
 	 glViewport(0, 0, w, h);
 	 //reset the window width and height size here so that each view point is different
 	 m_winW = w;
 	 m_winH = h;
+
+	glLoadIdentity();
+    glOrtho(-xmax, xmax, -ymax, ymax, -1.0, 1.0);
 
 	// PUT YOUR CODE HERE
 }
@@ -101,13 +116,11 @@ HW1a::resizeGL(int w, int h)
 void
 HW1a::paintGL()
 {
-	//I think  we need to call the resize function here so that we have a specific instance of the viewport but I don't get how
+	
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	//glMatrixMode(GL_MODELVIEW);
-	//glLoadIdentity();
-
-	int WidthBuffer = m_winW / 3;
+	//set the viewport size from the whole window size
+	int WidthBuffer = m_winW / 3; 
 	int HeightBuffer = m_winH / 3;
 	int index = 0;
 
@@ -116,23 +129,10 @@ HW1a::paintGL()
 			//set the viewport
             glViewport(c * WidthBuffer, r * HeightBuffer, WidthBuffer, HeightBuffer);
 
-			//clear the scene for this specifying viewport drawing 
-			glEnable(GL_SCISSOR_TEST);
-			glScissor(c * WidthBuffer, r * HeightBuffer, WidthBuffer, HeightBuffer);
-
-			glClear(GL_COLOR_BUFFER_BIT);
-
-			glDisable(GL_SCISSOR_TEST);
-
             // Reset projection
             glMatrixMode(GL_PROJECTION);
             glLoadIdentity();
             glOrtho(-1, 1, -1, 1, -1, 1);
-
-            // Reset modelview
-            glMatrixMode(GL_MODELVIEW);
-            glLoadIdentity();
-			//the above reset the positioning of the drawing in the given frame
 
             // Draw
             glBegin(DrawModes[index]);
